905deaf3a43710f995e63b336edc24e9417a0b9b
Lukas Brähler <58339175+cheekzi@users.noreply.github.com>
2021-08-18 17:21:35 +0200

    update

--- cogs/itemshop.py
+++ cogs/itemshop.py
@@ -18,7 +18,7 @@ class itemshop(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
             
-        if message.author != discord.Client().user:
+        if !(message.author.bot):
             print(message.content)
             if not message.guild:
                 
