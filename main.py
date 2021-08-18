2f18e7d7638654ff055acce5a656e375655c843c
Lukas Brähler <58339175+cheekzi@users.noreply.github.com>
2021-08-18 18:18:29 +0200

    online status

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def create_db_pool():
 @bot.event
 async def on_ready():
     await bot.change_presence(
-        status=discord.Status.idle, 
+        status=discord.Status.online, 
         activity=discord.Activity(type=discord.ActivityType.watching, name=f"{prefix}help")
     )
     print("Bot online!")
