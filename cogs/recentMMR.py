bfa50666947b17e67156975286d32d7dd4441221
Lukas Brähler <58339175+cheekzi@users.noreply.github.com>
2021-08-18 21:18:08 +0200

    update

--- cogs/recentMMR.py
+++ cogs/recentMMR.py
@@ -14,70 +14,39 @@ class rank(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def mmr(self, ctx):
+    async def mmr(self, ctx, name:str):
         author_id = str(ctx.author.id)
         
-        try:
+        if not name:
             user = await self.client.pg_con.fetchrow("SELECT * FROM riotpwd WHERE user_id = $1", author_id)
             username = user['username']
             password = user['password']
             region   = user['region']
-
-            try: 
-                if user:
-                    await ctx.send("Loading rank...")
-                    user_data = username_to_data(username, password)
-                    user_id = user_data[2]
-                    current_rank = getrank(region,user_id)
-
-
-                    player_rank = current_rank['data']['current_data']['currenttierpatched']
-                    current_tier = current_rank['data']['current_data']['currenttier']
-                    ranking_in_tier = current_rank['data']['current_data']['ranking_in_tier']
-                    mmr_change_to_last_game = current_rank['data']['current_data']['mmr_change_to_last_game']
-                    games_needed_for_rating = current_rank['data']['current_data']['games_needed_for_rating']
-
-                    embed = discord.Embed(
-                        color=0xFF9B0A,
-                        title="RANK"
-                    )
-                    embed.add_field(name ="Current Rank",value=f"{player_rank}",inline=False)
-                    embed.add_field(name ="Ranking in Tier",value=f"{ranking_in_tier}",inline=False)
-                    embed.add_field(name ="Games needed for rating",value=f"{games_needed_for_rating}",inline=False)
-                    embed.set_thumbnail(url=f"https://raw.githubusercontent.com/typhonshambo/Valorant-server-stat-bot/main/assets/valorantRankImg/{current_tier}.png")
-
-
-
-                    await ctx.send(embed=embed)
-            except:
-                embed= discord.Embed(
-                    color=discord.Color.red()
-                )
-                embed.add_field(name ="SOME ERROR OCCURED...",value="""
-                Either your `username` \nor your `password` \nor your `region` is incorrect.
-                """,inline=False)
+            
+            try:
+                user_data = username_to_data(username, password)
+                user_id = user_data[2]
+                mmrHistory = getMMRHistory(region, user_id)
+                for i in range 5:
+                    rank = mmrHistory["data"][i]["currenttierpatched"]
+                    mmr = mmrHistory["data"][i]["ranking_in_tier"]
+                    change = mmrHistory["data"][i]["mmr_change_to_last_game"]
+                    ctx.send(rank + str(mmr) + str(change))
         
-                embed.set_thumbnail(url="https://i.imgur.com/A45DVhf.gif")
-                await ctx.send(
-                    embed=embed,
-                    components=[
-                        [
-                            Button(label="Support Server", style=5, url="https://discord.gg/m5mSyTV7RR"),
-                            Button(label="Vote", style=5, url="https://top.gg/bot/864451929346539530/vote")
-                        ]
-                    ]
-                )
+        else:
+            username = message.content.split('#')
+            name=username[0]
+            tag=username[1]
+            
+            try:
+                mmrHistory = getMMRHistory_name(name, tag)
+                for i in range 5:
+                    rank = mmrHistory["data"][i]["currenttierpatched"]
+                    mmr = mmrHistory["data"][i]["ranking_in_tier"]
+                    change = mmrHistory["data"][i]["mmr_change_to_last_game"]
+                    ctx.send(rank + str(mmr) + str(change))
 
-        except:
-            embed = discord.Embed(
-                color= discord.Color.red()
-            )
-            embed.add_field(name ="HOLD ON MAN !",value = f"""
-            you need to login to your account before you can use this command,
-            use `{prefix}login` to login to your account
-            """)
-            await ctx.send(embed=embed)
 def setup(client):
-    client.add_cog(rank(client))
+    client.add_cog(recentMMR(client))
     print("recentMMR         | Imported") 
 
