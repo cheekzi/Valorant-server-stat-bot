48de80f96c688cff6f7bc3384b4d246e7b477fff
Lukas Brähler <58339175+cheekzi@users.noreply.github.com>
2021-08-22 20:31:49 +0200

    update

--- cogs/rank.py
+++ cogs/rank.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import requests
 import json 
-from .utils.rank_utils import username_to_data,getrank
+from .utils.rank_utils import username_to_data,getrank, getrank_name
 from discord_components import *
 
 with open ('././config/config.json', 'r') as f:
@@ -14,7 +14,7 @@ class rank(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def rank(self, ctx):
+    async def rank(self, ctx, *, name=None):
         author_id = str(ctx.author.id)
         
         try:
@@ -22,61 +22,97 @@ class rank(commands.Cog):
             username = user['username']
             password = user['password']
             region   = user['region']
+            
+            if not name:
+                try: 
+                    if user:
+                        await ctx.send("Loading rank...")
+                        user_data = username_to_data(username, password)
+                        user_id = user_data[2]
+                        current_rank = getrank(region,user_id)
 
-            try: 
-                if user:
-                    await ctx.send("Loading rank...")
-                    user_data = username_to_data(username, password)
-                    user_id = user_data[2]
-                    current_rank = getrank(region,user_id)
+
+                        player_rank = current_rank['data']['current_data']['currenttierpatched']
+                        current_tier = current_rank['data']['current_data']['currenttier']
+                        ranking_in_tier = current_rank['data']['current_data']['ranking_in_tier']
+                        mmr_change_to_last_game = current_rank['data']['current_data']['mmr_change_to_last_game']
+                        games_needed_for_rating = current_rank['data']['current_data']['games_needed_for_rating']
+
+                        embed = discord.Embed(
+                            color=0xFF9B0A,
+                            title="RANK"
+                        )
+                        embed.add_field(name ="Current Rank",value=f"{player_rank}",inline=False)
+                        embed.add_field(name ="Ranking in Tier",value=f"{ranking_in_tier}",inline=False)
+                        embed.add_field(name ="Games needed for rating",value=f"{games_needed_for_rating}",inline=False)
+                        embed.set_thumbnail(url=f"https://raw.githubusercontent.com/typhonshambo/Valorant-server-stat-bot/main/assets/valorantRankImg/{current_tier}.png")
 
 
-                    player_rank = current_rank['data']['current_data']['currenttierpatched']
-                    current_tier = current_rank['data']['current_data']['currenttier']
-                    ranking_in_tier = current_rank['data']['current_data']['ranking_in_tier']
-                    mmr_change_to_last_game = current_rank['data']['current_data']['mmr_change_to_last_game']
-                    games_needed_for_rating = current_rank['data']['current_data']['games_needed_for_rating']
 
-                    embed = discord.Embed(
-                        color=0xFF9B0A,
-                        title="RANK"
+                        await ctx.send(embed=embed)
+                except:
+                    embed= discord.Embed(
+                        color=discord.Color.red()
                     )
-                    embed.add_field(name ="Current Rank",value=f"{player_rank}",inline=False)
-                    embed.add_field(name ="Ranking in Tier",value=f"{ranking_in_tier}",inline=False)
-                    embed.add_field(name ="Games needed for rating",value=f"{games_needed_for_rating}",inline=False)
-                    embed.set_thumbnail(url=f"https://raw.githubusercontent.com/typhonshambo/Valorant-server-stat-bot/main/assets/valorantRankImg/{current_tier}.png")
+                    embed.add_field(name ="SOME ERROR OCCURED...",value="""
+                    Either your `username` \nor your `password` \nor your `region` is incorrect.
+                    """,inline=False)
 
+                    embed.set_thumbnail(url="https://i.imgur.com/A45DVhf.gif")
+                    await ctx.send(
+                        embed=embed,
+                        components=[
+                            [
+                                Button(label="Support Server", style=5, url="https://discord.gg/m5mSyTV7RR"),
+                                Button(label="Vote", style=5, url="https://top.gg/bot/864451929346539530/vote")
+                            ]
+                        ]
+                    )
 
-
-                    await ctx.send(embed=embed)
             except:
+                embed = discord.Embed(
+                    color= discord.Color.red()
+                )
+                embed.add_field(name ="HOLD ON MAN !",value = f"""
+                you need to login to your account before you can use this command,
+                use `{prefix}login` to login to your account
+                """)
+                await ctx.send(embed=embed)
+        else:
+            try:
+                username = name.split('#')           
+                current_rank = getrank_name(username[0], username[1])
+                
+                player_rank = current_rank['data']['current_data']['currenttierpatched']
+                current_tier = current_rank['data']['current_data']['currenttier']
+                ranking_in_tier = current_rank['data']['current_data']['ranking_in_tier']
+                mmr_change_to_last_game = current_rank['data']['current_data']['mmr_change_to_last_game']
+                games_needed_for_rating = current_rank['data']['current_data']['games_needed_for_rating']
+
+                embed = discord.Embed(
+                    color=0xFF9B0A,
+                    title="RANK"
+                )
+                embed.add_field(name ="Current Rank",value=f"{player_rank}",inline=False)
+                embed.add_field(name ="Ranking in Tier",value=f"{ranking_in_tier}",inline=False)
+                embed.add_field(name ="Games needed for rating",value=f"{games_needed_for_rating}",inline=False)
+                embed.set_thumbnail(url=f"https://raw.githubusercontent.com/typhonshambo/Valorant-server-stat-bot/main/assets/valorantRankImg/{current_tier}.png")
+
+                await ctx.send(embed=embed)
+                       
+            except Exception as e:
+                print(e)
                 embed= discord.Embed(
                     color=discord.Color.red()
                 )
-                embed.add_field(name ="SOME ERROR OCCURED...",value="""
-                Either your `username` \nor your `password` \nor your `region` is incorrect.
+                embed.add_field(name ="SOME ERROR OCCURED...",value=f"""
+                Couldn't find statistics for `{name}` \n Check if the name is correct and try again later.
                 """,inline=False)
-        
                 embed.set_thumbnail(url="https://i.imgur.com/A45DVhf.gif")
                 await ctx.send(
-                    embed=embed,
-                    components=[
-                        [
-                            Button(label="Support Server", style=5, url="https://discord.gg/m5mSyTV7RR"),
-                            Button(label="Vote", style=5, url="https://top.gg/bot/864451929346539530/vote")
-                        ]
-                    ]
+                    embed=embed
                 )
 
-        except:
-            embed = discord.Embed(
-                color= discord.Color.red()
-            )
-            embed.add_field(name ="HOLD ON MAN !",value = f"""
-            you need to login to your account before you can use this command,
-            use `{prefix}login` to login to your account
-            """)
-            await ctx.send(embed=embed)
 def setup(client):
     client.add_cog(rank(client))
     print("rank         | Imported")   
