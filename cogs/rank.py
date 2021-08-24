a07bb325656c84d40aaa1d973ee59acf73b301c7
Lukas Brähler <58339175+cheekzi@users.noreply.github.com>
2021-08-24 19:58:23 +0200

    update

--- cogs/rank.py
+++ cogs/rank.py
@@ -42,7 +42,8 @@ class rank(commands.Cog):
                             title="RANK"
                         )
                         embed.add_field(name ="Current Rank",value=f"{player_rank}",inline=False)
-                        embed.add_field(name ="Ranking in Tier",value=f"{ranking_in_tier}",inline=False)
+                        embed.add_field(name ="Ranking in Tier",value=f"{ranking_in_tier}",true)
+                        embed.add_field(name ="Last Game MMR Change",value=f"{mmr_change_to_last_game}",true)
                         embed.add_field(name ="Games needed for rating",value=f"{games_needed_for_rating}",inline=False)
                         embed.set_thumbnail(url=f"https://raw.githubusercontent.com/typhonshambo/Valorant-server-stat-bot/main/assets/valorantRankImg/{current_tier}.png")
 
@@ -92,7 +93,8 @@ class rank(commands.Cog):
                     title="RANK"
                 )
                 embed.add_field(name ="Current Rank",value=f"{player_rank}",inline=False)
-                embed.add_field(name ="Ranking in Tier",value=f"{ranking_in_tier}",inline=False)
+                embed.add_field(name ="Ranking in Tier",value=f"{ranking_in_tier}",true)
+                embed.add_field(name ="Last Game MMR Change",value=f"{mmr_change_to_last_game}",true)
                 embed.set_thumbnail(url=f"https://raw.githubusercontent.com/typhonshambo/Valorant-server-stat-bot/main/assets/valorantRankImg/{current_tier}.png")
 
                 await ctx.send(embed=embed)
