fd838a36bd139b70c000cea5fb621d0141229a39
Lukas Brähler <58339175+cheekzi@users.noreply.github.com>
2021-09-01 22:08:00 +0200

    update

---
+++ cogs/graph.py
@@ -0,0 +1,127 @@
+import discord
+from discord.ext import commands
+import requests
+import json 
+from .utils.rank_utils import username_to_data,getMMRHistory, getMMRHistory_name
+from discord_components import *
+from datetime import datetime
+
+with open ('././config/config.json', 'r') as f:
+    config = json.load(f, strict=False)
+    prefix = config['prefix']
+
+class graph(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def graph(self, ctx, *, name=None):
+        author_id = str(ctx.author.id)
+        
+        if not name:
+            
+            try:
+                user = await self.client.pg_con.fetchrow("SELECT * FROM riotpwd WHERE user_id = $1", author_id)
+                username = user['username']
+                password = user['password']
+                region   = user['region']
+                user_data = username_to_data(username, password)
+                user_id = user_data[2]
+                mmrHistory = getMMRHistory(region, user_id)
+                for i in range(5):
+                    rank = mmrHistory["data"][i]["currenttierpatched"]
+                    raw_rank = mmrHistory["data"][i]["currenttier"]
+                    mmr = mmrHistory["data"][i]["ranking_in_tier"]
+                    change = mmrHistory["data"][i]["mmr_change_to_last_game"]
+                    date = mmrHistory["data"][i]["date_raw"]
+                    
+                    if change < 0:
+                        colorx = discord.Color.red()
+                    else:
+                        colorx = discord.Color.green()
+                        change = f"+{change}"
+                        
+                    rr = f"**{mmr} / 100** RR            - **{change}**"
+                        
+                    embed = discord.Embed(
+                        title=rank,
+                        color = colorx,
+                        timestamp= datetime.fromtimestamp(date//1000)
+                    )
+
+                    embed.set_thumbnail(url=f"https://raw.githubusercontent.com/typhonshambo/Valorant-server-stat-bot/main/assets/valorantRankImg/{raw_rank}.png")
+                    embed.add_field(name="Rank Rating",value=rr,inline=False)
+                    
+                    footer = (
+                        "🟢 gespielt"
+                    )
+                    embed.set_footer(text=footer)
+
+                    await ctx.send(embed=embed)
+            
+            except Exception as e:
+                print(e)
+                embed= discord.Embed(
+                    color=discord.Color.red()
+                )
+                embed.add_field(name ="SOME ERROR OCCURED...",value="""
+                Couldn't find statistics. \n You have to be `logged in`to use this command \n Otherwise try using **!mmr <name#tag>** to view anyone's statistics.
+                """,inline=False)
+                embed.set_thumbnail(url="https://i.imgur.com/A45DVhf.gif")
+                await ctx.send(
+                    embed=embed
+                )
+        else:
+            print(name)
+            username = name.split('#')
+            tag=username[1]
+            username = username[0]
+            
+            try:
+                mmrHistory = getMMRHistory_name(username, tag)
+                for i in range(5):
+                    rank = mmrHistory["data"][i]["currenttierpatched"]
+                    raw_rank = mmrHistory["data"][i]["currenttier"]
+                    mmr = mmrHistory["data"][i]["ranking_in_tier"]
+                    change = mmrHistory["data"][i]["mmr_change_to_last_game"]
+                    date = mmrHistory["data"][i]["date_raw"]
+                    
+                    if change < 0:
+                        colorx = discord.Color.red()
+                    else:
+                        colorx = discord.Color.green()
+                        change = f"+{change}"
+                        
+                    rr = f"**{mmr} / 100** RR            - **{change}**"
+                        
+                    embed = discord.Embed(
+                        title=rank,
+                        color = colorx,
+                        timestamp= datetime.fromtimestamp(date//1000)
+                    )
+                    embed.set_thumbnail(url=f"https://raw.githubusercontent.com/typhonshambo/Valorant-server-stat-bot/main/assets/valorantRankImg/{raw_rank}.png")
+                    embed.add_field(name="Rank Rating",value=rr,inline=False)
+
+                    footer = (
+                        "🟢 gespielt"
+                    )
+                    embed.set_footer(text=footer)
+                    
+                    await ctx.send(embed=embed)
+                       
+            except Exception as e:
+                print(e)
+                embed= discord.Embed(
+                    color=discord.Color.red()
+                )
+                embed.add_field(name ="SOME ERROR OCCURED...",value=f"""
+                Couldn't find statistics for `{name}` \n Check if the name is correct and try again later.
+                """,inline=False)
+                embed.set_thumbnail(url="https://i.imgur.com/A45DVhf.gif")
+                await ctx.send(
+                    embed=embed
+                )
+
+def setup(client):
+    client.add_cog(graph(client))
+    print("Graph      | Imported") 
