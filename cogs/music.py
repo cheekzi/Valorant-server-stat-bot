e151f46c5dc755b2fa6f6f2444c54fb2992e5c27
Ante Brähler <58339175+cheekzi@users.noreply.github.com>
2021-09-19 22:37:40 +0200

    test

--- cogs/music.py
+++ cogs/music.py
@@ -25,10 +25,10 @@ class music(commands.Cog):
 
     # Initiate our nodes. For this example we will use one server.
     # Region should be a discord.py guild.region e.g sydney or us_central (Though this is not technically required)
-    await self.bot.wavelink.initiate_node("id": "Main",
-    "host": "lavalink.darrennathanael.com",
-    "port": 2095,
-    "pass": "whatwasthelastingyousaid",)
+    await self.bot.wavelink.initiate_node(id='Main',
+    host='lavalink.darrennathanael.com',
+    port=2095,
+    pass='whatwasthelastingyousaid')
 
     
   @commands.command(name='connect')
